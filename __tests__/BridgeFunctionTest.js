6b81f8a265cf2a557f682f212d8e655933ddf3bf
gilpop8663 <wolfye@naver.com>
2022-12-14 05:40:17 +0900

    test(BridgeFunctionTest): 다리 건너기에 사용되는 함수 테스트

--- __tests__/BridgeFunctionTest.js
+++ __tests__/BridgeFunctionTest.js
@@ -1,6 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const App = require('../src/App');
-const BridgeMaker = require('../src/BridgeMaker');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -28,16 +27,6 @@ const getOutput = (logSpy) => {
   return [...logSpy.mock.calls].join('');
 };
 
-const runException = (inputs) => {
-  mockQuestions(inputs);
-  const logSpy = getLogSpy();
-  const app = new App();
-
-  app.play();
-
-  expectLogContains(getOutput(logSpy), ['[ERROR]']);
-};
-
 const expectLogContains = (received, logs) => {
   logs.forEach((log) => {
     expect(received).toEqual(expect.stringContaining(log));
@@ -51,21 +40,30 @@ const expectBridgeOrder = (received, upside, downside) => {
   expect(upsideIndex).toBeLessThan(downsideIndex);
 };
 
-describe('다리 건너기 테스트', () => {
-  test('다리 생성 테스트', () => {
-    const randomNumbers = ['1', '0', '0'];
-    const mockGenerator = randomNumbers.reduce((acc, number) => {
-      return acc.mockReturnValueOnce(number);
-    }, jest.fn());
-
-    const bridge = BridgeMaker.makeBridge(3, mockGenerator);
-    expect(bridge).toEqual(['U', 'D', 'D']);
-  });
-
-  test('기능 테스트', () => {
+describe('다리 건너기에 사용되는 함수들이 올바르게 작동되는 지 테스트', () => {
+  test('게임 실패 테스트', () => {
     const logSpy = getLogSpy();
     mockRandoms(['1', '0', '1']);
-    mockQuestions(['3', 'U', 'D', 'U']);
+    mockQuestions(['3', 'U', 'D', 'D', 'Q']);
+
+    const app = new App();
+    app.play();
+
+    const log = getOutput(logSpy);
+    expectLogContains(log, [
+      '최종 게임 결과',
+      '[ O |   |   ]',
+      '[   | O | X ]',
+      '게임 성공 여부: 실패',
+      '총 시도한 횟수: 1',
+    ]);
+    expectBridgeOrder(log, '[ O |   |   ]', '[   | O | X ]');
+  });
+
+  test('게임 재시작 테스트', () => {
+    const logSpy = getLogSpy();
+    mockRandoms(['1', '0', '1']);
+    mockQuestions(['3', 'U', 'D', 'D', 'R', 'U']);
 
     const app = new App();
     app.play();
@@ -76,12 +74,8 @@ describe('다리 건너기 테스트', () => {
       '[ O |   | O ]',
       '[   | O |   ]',
       '게임 성공 여부: 성공',
-      '총 시도한 횟수: 1',
+      '총 시도한 횟수: 2',
     ]);
     expectBridgeOrder(log, '[ O |   | O ]', '[   | O |   ]');
-  });
-
-  test('예외 테스트', () => {
-    runException(['a']);
   });
 });
